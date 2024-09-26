4750183dab05ad2cfa928f1fa4623967b6ffae25
Hikmatullohprogramist <tillo2439@gmail.com>
2024-09-26 10:55:51 +0500

    Initial commit

---
+++ bin/instagram_downloader_bot.dart
@@ -0,0 +1,5 @@
+import 'package:instagram_downloader_bot/instagram_downloader_bot.dart' as instagram_downloader_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${instagram_downloader_bot.calculate()}!');
+}
---
+++ test/instagram_downloader_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:instagram_downloader_bot/instagram_downloader_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
